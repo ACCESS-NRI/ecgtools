f3fa76df9240a06ba2da643d634af8f3de65675b
Charles Turner <charles.turner@anu.edu.au>
2025-09-24 10:18:18 +0800

    Remove some unnecessary versioning stuff (old)

--- ecgtools/__init__.py
+++ ecgtools/__init__.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python
 # flake8: noqa
 """Top-level module for ecgtools ."""
-from importlib.metadata import PackageNotFoundError, version
 
 from .builder import Builder, RootDirectory, glob_to_regex
 
-try:
-    __version__ = version(__name__)
-except PackageNotFoundError:
-    # package is not installed
-    __version__ = '0.0.0'
-
 from . import _version
-__version__ = _version.get_versions()['version']
+
+__version__ = _version.get_versions()["version"]
